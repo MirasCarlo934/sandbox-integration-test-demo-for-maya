10c9fbddd12b9fc5e70d7ce8a0a6ede411052469
Carlo Miras <mirascarlo934@gmail.com>
2023-04-29 11:57:20 +0800

    Add liquibase

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
+# set up db
+mvn liquibase:update
+
+# set up wiremock stubs
 cd src/test/resources/stubs
 curl -X POST localhost:8080/__admin/mappings -H "Content-Type: application/json" -d @walletservice-create-new-wallet-200.json
