f8277f525e6f47b5eac8b505cf129c3aa99ccc56
Carlo Miras <carlo.miras@paymaya.com>
2023-05-02 10:01:48 +0800

    Change values for work laptop environment

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ cd ../../../
 
 # set up wiremock stubs
 cd src/test/resources/stubs
-curl -X POST localhost:8080/__admin/mappings -H "Content-Type: application/json" -d @walletservice-create-new-wallet-200.json
+curl -X DELETE http://localhost:7000/__admin/mappings
+curl -X POST localhost:7000/__admin/mappings -H "Content-Type: application/json" -d @walletservice-create-new-wallet-200.json
 cd ../../../../
