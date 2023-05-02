90b89781975f079303e77fd8635913614afd826b
Carlo Miras <carlo.miras@paymaya.com>
2023-05-02 14:47:58 +0800

    Add SNS and PersonCreatedEventSnsPublisher config to ApplicationConfig

--- src/main/java/com/example/integrationtestdemoformaya/config/ApplicationConfig.java
+++ src/main/java/com/example/integrationtestdemoformaya/config/ApplicationConfig.java
@@ -1,5 +1,13 @@
 package com.example.integrationtestdemoformaya.config;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.example.integrationtestdemoformaya.events.dto.PersonCreatedEvent;
+import com.example.integrationtestdemoformaya.events.publisher.PersonCreatedEventSnsPublisher;
+import com.example.integrationtestdemoformaya.events.publisher.SnsPublisher;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +18,38 @@ import java.util.Map;
 
 @Configuration
 public class ApplicationConfig {
+    private final String snsEndpoint;
+    private final String snsRegion;
+    private final String personsSnsTopicArn;
+    private final ObjectMapper objectMapper;
+
+    public ApplicationConfig(
+            @Value("${aws.sns.endpoint}") String snsEndpoint,
+            @Value("${aws.sns.region}") String snsRegion,
+            @Value("${aws.sns.topic-arns.persons}") String personsSnsTopicArn,
+            ObjectMapper objectMapper) {
+        this.snsEndpoint = snsEndpoint;
+        this.snsRegion = snsRegion;
+        this.personsSnsTopicArn = personsSnsTopicArn;
+        this.objectMapper = objectMapper;
+    }
+
+    @Bean
+    public SnsPublisher<PersonCreatedEvent> personCreatedEventSnsPublisher() {
+        return new PersonCreatedEventSnsPublisher(amazonSNS(), personsSnsTopicArn, objectMapper);
+    }
+
     @Bean
     public WebClient walletWebClient() {
         return WebClient.builder()
                 .baseUrl(walletServiceEndpointProperties().toUrl())
+                .build();
+    }
+
+    @Bean
+    public AmazonSNS amazonSNS() {
+        return AmazonSNSClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(snsEndpoint, snsRegion))
                 .build();
     }
 
--- src/main/java/com/example/integrationtestdemoformaya/events/publisher/PersonCreatedEventSnsPublisher.java
+++ src/main/java/com/example/integrationtestdemoformaya/events/publisher/PersonCreatedEventSnsPublisher.java
@@ -4,22 +4,20 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.example.integrationtestdemoformaya.events.dto.PersonCreatedEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import org.springframework.stereotype.Component;
 
-@Component
 public class PersonCreatedEventSnsPublisher extends SnsPublisher<PersonCreatedEvent> {
-    private final String personSnsTopicArn;
+    private final String personsSnsTopicArn;
     private final ObjectMapper objectMapper;
 
-    public PersonCreatedEventSnsPublisher(AmazonSNS amazonSNS, String personSnsTopicArn, ObjectMapper objectMapper) {
+    public PersonCreatedEventSnsPublisher(AmazonSNS amazonSNS, String personsSnsTopicArn, ObjectMapper objectMapper) {
         super(amazonSNS);
-        this.personSnsTopicArn = personSnsTopicArn;
+        this.personsSnsTopicArn = personsSnsTopicArn;
         this.objectMapper = objectMapper;
     }
 
     @Override
     @SneakyThrows
     public void publish(PersonCreatedEvent event) {
-        publish(personSnsTopicArn, objectMapper.writeValueAsString(event));
+        publish(personsSnsTopicArn, objectMapper.writeValueAsString(event));
     }
 }
